7a14f86ca73e32bba973da96a2d08323dcb705c9
PhamVuHuy99 <huy122k@gmail.com>
2022-04-05 22:04:45 +0700

    Initital Commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,7 +133,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('hello');
 };
 
 init();
